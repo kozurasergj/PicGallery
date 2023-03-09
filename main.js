a7202fd105e0113d961d737cd70a30cb9a7c53b2
kozurasergj <kozura.sergj33@gmail.com>
2023-03-09 18:15:12 +0200

    added main

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-import { modals } from "./modules";
+import { modals, sliders } from "./modules";
 
 window.addEventListener('DOMContentLoaded', () => {
   modals();
-
+  sliders({
+    slide: '.feedback-slider-item',
+    direction: '',
+    preview: '.main-prev-btn',
+    next: '.main-next-btn'
+  });
+  sliders({
+    slide: '.main-slider-item',
+    direction: 'vertical',
+    preview: '',
+    next: ''
+  });
 });
