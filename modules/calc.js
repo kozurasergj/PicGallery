a322dc02dd05e94c2124f0b39cc80f1dec756bfd
kozurasergj <kozura.sergj33@gmail.com>
2023-03-13 18:10:59 +0200

    Promocode  toUpperCase()

--- modules/calc.js
+++ modules/calc.js
@@ -10,13 +10,12 @@ export const calc = (size, material, options, promocode, result) => {
     sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
     if (sizeBlock.value == '' || materialBlock.value == '') {
       resultBlock.textContent = "Пожалуйста, выбирите размер и материал картины"
-    } else if (promocodeBlock.value == 'IWANTPOPART') {
+    } else if (promocodeBlock.value.toUpperCase() == 'IWANTPOPART') {
       resultBlock.textContent = Math.round(sum * 0.7);
     } else {
       resultBlock.textContent = sum;
     }
   }
-  
   sizeBlock.addEventListener('change', calcFunc);
   materialBlock.addEventListener('change', calcFunc);
   optionsBlock.addEventListener('change', calcFunc);
